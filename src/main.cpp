db1d626586ac57b767042e652cc02fbff7cef85b
Bartosz Piątek <bartekpiatek123@gmail.com>
2026-06-13 20:09:29 +0200

    window, code formatting and cmake work

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include "raylib.h"
 
+inline constexpr int k_window_size_x = 1920;
+inline constexpr int k_window_size_y = 1080;
+inline constexpr int k_target_fps = 160;
+
 int main() {
-    InitWindow(1280, 720, "LoL Practice Tool");
+    InitWindow(k_window_size_x, k_window_size_y, "Practice Tool");
+    SetTargetFPS(k_target_fps);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(Color{18, 18, 22, 255});
+        EndDrawing();
+    }
+
     CloseWindow();
     return 0;
 }
